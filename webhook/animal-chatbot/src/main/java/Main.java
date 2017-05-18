7b499061e42957e4f5edfc7e28e19965d62c91f4
Rafael Avelar Pacheco <rafaelpa@takenet.com.br>
2017-05-17 23:51:04 -0300

    Add bucket command for ping messages

--- webhook/animal-chatbot/src/main/java/Main.java
+++ webhook/animal-chatbot/src/main/java/Main.java
@@ -1,8 +1,6 @@
 import LimeConverter.LimeConverterFactory;
 import okhttp3.ResponseBody;
-import org.limeprotocol.Message;
-import org.limeprotocol.Node;
-import org.limeprotocol.Notification;
+import org.limeprotocol.*;
 import org.limeprotocol.messaging.contents.PlainText;
 import org.limeprotocol.serialization.JacksonEnvelopeSerializer;
 import retrofit2.Call;
@@ -10,12 +8,25 @@ import retrofit2.Callback;
 import retrofit2.Retrofit;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
 
 public class Main {
     private static JacksonEnvelopeSerializer target;
+
+    private static final Callback<ResponseBody> requestCallback = new Callback<ResponseBody>() {
+        @Override
+        public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
+            System.out.println(response.body());
+        }
+
+        @Override
+        public void onFailure(Call<ResponseBody> call, Throwable t) {
+            System.out.println(t);
+        }
+    };
 
     public static void main(String[] args) {
 
@@ -38,20 +49,26 @@ public class Main {
             Node from = message.getFrom();
             message.setFrom(message.getTo());
             message.setTo(from);
-            message.setContent(new PlainText("Hello from Webhook Java"));
+
+            if(message.getType().toString().equals(PlainText.MIME_TYPE) &&
+                    ((PlainDocument)message.getContent()).getValue().toLowerCase().equals("ping")){
+                Command resetSession = new Command();
+                resetSession.setId(UUID.randomUUID().toString());
+                resetSession.setMethod(Command.CommandMethod.DELETE);
+                //Only for test!!!
+                resetSession.setUri(LimeUri.parse("/buckets/" + from.toString().split("@")[0]));
+
+                Call<ResponseBody> r = blipService.sendCommands(resetSession);
+                r.enqueue(requestCallback);
+
+                message.setContent(new PlainText("Pong!!!"));
+                blipService.sendMessage(message);
+            } else {
+                message.setContent(new PlainText("Hello from Webhook Java"));
+            }
 
             Call<ResponseBody> r = blipService.sendMessage(message);
-            r.enqueue(new Callback<ResponseBody>() {
-                @Override
-                public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
-                    System.out.println(response.body());
-                }
-
-                @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-                    System.out.println(t);
-                }
-            });
+            r.enqueue(requestCallback);
 
             response.status(HttpServletResponse.SC_OK);
             return response;
